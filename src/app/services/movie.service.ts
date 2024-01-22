9c57a7b91d6aff0774440c43e4878769eb662320
Mauricio <mauriciomage@gmail.com>
2024-01-22 15:02:34 +0100

    get movies top and details

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,9 +1,29 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { delay } from 'rxjs/operators';
+import { Observable } from 'rxjs/internal/Observable';
+import { environment } from 'src/environments/environment';
+import { ApiResult, MovieResult } from '../interfaces/interface';
+
+const BASE_URL = 'https://api.themoviedb.org/3';
+const API_KEY = environment.apiKey;
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MovieService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getTopRatedMovies(page = 1): Observable<ApiResult> {
+    return this.http
+      .get<ApiResult>(`${BASE_URL}/movie/popular?page=${page}&api_key=${API_KEY}`)
+      .pipe(
+       //  delay(2000) // Simulate slow network
+      );
+  }
+
+  getMovieDetails(id: string): Observable<MovieResult> {
+    return this.http.get<MovieResult>(`${BASE_URL}/movie/${id}?api_key=${API_KEY}`);
+  }
 }
